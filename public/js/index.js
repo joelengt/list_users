fb1c187a75f7413bebf3c9e9859ca3e6c764e66f
joelengt <joelengt@gmail.com>
2016-11-15 01:45:54 -0500

    CSS table style

--- public/js/index.js
+++ public/js/index.js
@@ -19,13 +19,13 @@
         buildUserTemplate() {
             // Template de Usuario
             let template_user_item = `<tr class="itemUser" data-id="${ this.id }">
-                                        <td>${ this.id }</td>
-                                        <td>${ this.name }</td>
-                                        <td>${ this.last_name }</td>
-                                        <td>${ this.address }</td>
-                                        <td>${ this.cellphone }</td>
-                                        <td>${ this.telephone }</td>
-                                        <td><img class="imagenAvatar" src='./images/${ this.avatar }' alt="${ this.name } ${ this.last_name }"height="40"></td>
+                                        <td class="text-center">${ this.id }</td>
+                                        <td class="text-center">${ this.name }</td>
+                                        <td class="text-center">${ this.last_name }</td>
+                                        <td class="text-center">${ this.address }</td>
+                                        <td class="text-center">${ this.cellphone }</td>
+                                        <td class="text-center">${ this.telephone }</td>
+                                        <td class="text-center"><img class="imagenAvatar" src='./images/${ this.avatar }' alt="${ this.name } ${ this.last_name }"height="40"></td>
                                       </tr>`;
             return template_user_item
         }
@@ -44,6 +44,7 @@
 
     // Recorriendo lista obtenida
     function runList(array, limitStart, limitEnd, contentHtml) {
+        contentHtml.innerHTML = '';
         // Evento ciclo
         for(var i = limitStart; i <= limitEnd; i++) {
             var elemento_usuario = array[i];
@@ -65,6 +66,7 @@
             success: function (listUsuarios) {
 
                 contentHtml.innerHTML = '';
+
                 getPaginationTemplate(limitEachPage, listUsuarios.result.length);
                 var valueInit = 0;
                 var valueEnd = 9;
@@ -83,6 +85,8 @@
             url: `/dashboard/usuarios-list/item/0/${ user_id }`,
             method: 'get',
             success: function (usuario) {
+
+                contentHtml.innerHTML = '';
 
                 // Recorre lista y render Template en html
                 let nuevoUser = getUser(usuario.result);
@@ -246,27 +250,50 @@
 
     }
 
-    // Nav Fixed and scroll event
-    function ScrollNav() {
-        window.onscroll = changePos;
+    function goheadfixed(classtable) {
+    
+        if($(classtable).length) {
+    
+            $(classtable).wrap('<div class="fix-inner"></div>'); 
+            $('.fix-inner').wrap('<div class="fix-outer" style="position:relative; margin:auto;"></div>');
+            $('.fix-outer').append('<div class="fix-head"></div>');
+            $('.fix-head').prepend($('.fix-inner').html());
+            $('.fix-head table').find('caption').remove();
+            $('.fix-head table').css('width','100%');
+    
+            $('.fix-outer').css('width', $('.fix-inner table').outerWidth(true)+'px');
+            $('.fix-head').css('width', $('.fix-inner table').outerWidth(true)+'px');
+            $('.fix-head').css('height', $('.fix-inner table thead').height()+'px');
+    
+            // If exists caption, calculte his height for then remove of total
+            var hcaption = 0;
+            if($('.fix-inner table caption').length != 0)
+                hcaption = parseInt($('.fix-inner table').find('caption').height()+'px');
 
-        function changePos() {
-            var header = document.getElementById("header");
-            //var item_here = document.querySelector(".header__item");
+            // Table's Top
+            var hinner = parseInt( $('.fix-inner').offset().top );
 
-            if (window.pageYOffset > 90) {
-                header.style.position = "absolute";
-                header.style.top = pageYOffset + "px";
-                header.style.margin = "1rem";
-                //$(".header__item").css('background','red');
-                //$(".header__item").css('padding','0rem 2rem');
+            // Let's remember that <caption> is the beginning of a <table>, it mean that his top of the caption is the top of the table
+            $('.fix-head').css({'position':'absolute', 'overflow':'hidden', 'top': hcaption+'px', 'left':0, 'z-index':100 });
+        
+            $(window).scroll(function () {
+                var vscroll = $(window).scrollTop();
 
-
-            } else {
-                header.style.position = "";
-                header.style.top = "";
-            }
+                if(vscroll >= hinner + hcaption)
+                    $('.fix-head').css('top',(vscroll-hinner)+'px');
+                else
+                    $('.fix-head').css('top', hcaption+'px');
+            });
+    
+            /*  If the windows resize   */
+            $(window).resize(goresize);
+    
         }
+    }
+
+    function goresize() {
+        $('.fix-head').css('width', $('.fix-inner table').outerWidth(true)+'px');
+        $('.fix-head').css('height', $('.fix-inner table thead').outerHeight(true)+'px');
     }
 
     // Funcion Principal
@@ -286,8 +313,6 @@
          // Paginacion
         var limitePage = 10;
 
-        $boxConntentHtml.innerHTML = 'Cargando ...';
-
         // Lectura de Usuarios
         readUsers(limitePage, $boxConntentHtml);
 
@@ -306,6 +331,7 @@
 
        })
 
+       // Evento click -> Cambiar Orden
        $btn_change_order.addEventListener('click', function () {
             console.log('hi');
 
@@ -361,7 +387,7 @@
 
         })
 
-        ScrollNav()
+        goheadfixed('table.fixed')
 
     }
 
