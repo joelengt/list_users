1ae3c99979cb727ea695b3c737bbb2ad5deac9ee
joelengt <joelengt@gmail.com>
2016-11-15 12:02:54 -0500

    Favicon server

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cookieParser())
 app.use(methodOverride('_method'))
+app.use(favicon(path.join(__dirname, './public/images/favicon.png')))
 app.use(flash())
 
 
